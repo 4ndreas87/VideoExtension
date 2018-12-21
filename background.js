7efb240473be75ad9852fe0bd5bc51b6516bf084
kipmacsaigoren <kipmacsaigoren@gmail.com>
2018-12-21 14:26:50 -0800

    add popup.js, functionality for button

--- background.js
+++ background.js
@@ -5,7 +5,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
+  chrome.storage.sync.set({color: '#FFFF22'}, function() {
     console.log("The color is green.");
   });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
