8d84e0ef81c76a2aa3efddaa9ba272d534600d13
kipmacsaigoren <kipmacsaigoren@gmail.com>
2018-12-25 15:24:41 -0600

    closes current tab on video end

--- popup.js
+++ popup.js
@@ -1,53 +1,45 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
 let startService = document.getElementById('startService');
 
-
 function closeTab(activeTabID) {
-    // Closes current tab. This part works for sure, but nothing else does.
-    chrome.tabs.remove(ActiveTabID);
+    chrome.tabs.remove(activeTabID);
 };
 
 function addVidEndListener() {
 	chrome.tabs.query({'active': true, 'currentWindow': true}, function(t) {
 		// gets the current tab's integer ID
     	var tabID = t[0].id;
-    	// WHaTEVER YOU DO< DON"T USE DOCUMENT FOR SEARCHING THROUHG HTML because it'll only search the html of the 
-    	// popup and background
-    	// this is kinda a nonsense line
-    	// injects the 'code' string into the js of the page I think.
-    	// code string should get the first video element in the page, add a listener for it ending,
-    	// and, when it ends, close the tab.
+    	// inserts this code into the js of the current active tab
+    	// the code gets the first video element running on the page, and adds a listener it.
+    	// when the video ends it sends a message of bool true back to the extension 
+    	// (no need for extension ID because it isn't cross extension communication)
 		chrome.tabs.executeScript(tabID, {code:
 			"var vid = document.getElementsByTagName('video')[0]; \
 			vid.onended = function() { \
 				chrome.runtime.sendMessage(true); \
 			};"
-			// Not sure if I need the function at the end of sendmessage.
 		});
 	});
 };
 
-startService.addEventListener("click", addVidEndListener);
 
-function actOnVidEnd(ID) {
+function actOnVidEnd() {
+	// when the extension recieves the message that the video has ended (just true in this case)
+	// (might need to be changed later to confirm sender), It does func on the inputted tab.
 	chrome.runtime.onMessage.addListener(function(message, sender, func){
 		if (message == true) {
-			console.log("ASDFASDFADSFGSDFGSFGHSERTA$EWTRHWSGH")
-		}
+			// clicks all the links through until it gets to the next episode.
+			chrome.tabs.query({'active': true, 'currentWindow': true}, function(t) {
+				// gets the current tab's integer ID, closes it
+   				var tabID = t[0].id;
+   				closeTab(tabID);
+   			});
+		};
 	});
-} 
-
-// hopefully this works?
-chrome.tabs.query({'active': true, 'currentWindow': true}, function(t) {
-	// gets the current tab's integer ID
-   	var tabID = t[0].id;
-   	actOnVidEnd(tabID);
-});
+};
 
 
 
+
+startService.addEventListener('click', addVidEndListener);
+
+actOnVidEnd();
