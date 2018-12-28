bc9f228af206c5c608e0b1f75cd5a596dca0a1ec
kipmacsaigoren <kipmacsaigoren@gmail.com>
2018-12-28 17:13:07 -0600

    clicks through to new streamplay page. I hate asynchronicity

--- background.js
+++ background.js
@@ -29,58 +29,77 @@ function actOnVidEnd() {
 	// when the extension recieves the message that the video has ended (just true in this case)
 	// (might need to be changed later to confirm sender), closes the current (streamplay) tab
 	// hopefully later it will open the next video
+
+  /* so Here's why this is so long and so nestedly indented.
+  Kip can't figure out how to take these snippets out into being their own functions
+  because they all need to run after/as callbacks for the first one.
+  there might be a way to write them to run whatever callback I write to them
+  like 'closeWatchTab(callback = 
+          clickNextEp(callback = 
+            clickToPlayPage(callback = 
+              watchvid(callback = something ))));'
+  That's for later though. for now, lets just make it work.*/
+
 	chrome.runtime.onMessage.addListener(function(message, sender, func){
 		if (message == 'test sequence for message 4443211') {
 			// clicks all the links through until it gets to the next episode.
 			chrome.tabs.query({'active': true, 'currentWindow': true}, function(watchTab) {
 				// gets the current tab's integer ID, closes it
-   				var watchTabID = watchTab[0].id;
-   				chrome.tabs.remove(watchTabID);
+ 				var watchTabID = watchTab[0].id;
+ 				chrome.tabs.remove(watchTabID);
 
-   				chrome.tabs.query({'url': '*://*.swatchseries.to/episode*'}, function(lastEpTab) {
-   					// gets the watchseries tab's integer ID, clicks next episode and streamplay link
-   					// eventually change to whatever video domain you want.
-   					// put that in the options at some point
-   					var lastEpTabID = lastEpTab[0].id;
-   					chrome.tabs.executeScript(lastEpTabID, {code:
-   						"var nextButton = document.getElementsByClassName('npbutton button-next')[0]; \
-   						nextButton.click();"
-   					});
+ 				chrome.tabs.query({'url': '*://*.swatchseries.to/episode/*'}, function(lastEpTab) {
+          // gets the watchseries tab's integer ID, clicks next episode and streamplay link
+          // eventually change to whatever video domain you want.
+          // put that in the options at some point
+          var lastEpTabID = lastEpTab[0].id;
+          chrome.tabs.executeScript(lastEpTabID, {code:
+            "var nextButton = document.getElementsByClassName('npbutton button-next')[0]; \
+            nextButton.click();"
+          });
 
-   					// when the tab is upadated to the new episode screen, clicks the link to 
-   					// the first streamplay domain option.
-   					var domainSelectTabUpdated = false;
-   					chrome.tabs.onUpdated.addListener(function(domainSelectTabID, infoChange, domainSelectTab){
-   						if (domainSelectTabUpdated == false) {
-   							chrome.tabs.executeScript(domainSelectTabID, {code:
-   								// here is where we'd change the option to choose a different domain.
-   								"var domainSelectButton = document.querySelectorAll(\"[title = 'streamplay.to']\")[0]; \
-   								domainSelectButton.click();"
-   							});
-   							domainSelectTabUpdated = true;
-   						};
-   					});
+          // when the tab is upadated to the new episode screen, clicks the link to 
+          // the first streamplay domain option.
+          var domainSelectTabUpdated = false;
+          chrome.tabs.onUpdated.addListener(function(domainSelectTabID, infoChange, domainSelectTab){
+            if (domainSelectTabUpdated == false) {
+              chrome.tabs.executeScript(domainSelectTabID, 
 
-   					// here's my question, how do I identify the thisrd tab? is it always freecale?
-   					// It looks like it is but we'll have to see if I'm allowed to do that in the url field.
-   					// (Hopefully), gets the tab ID of the "click here to play" tab, and clicks here
-   					// to play.
-   					// so the problem here is that it's either checking for the new tab too fast and not getting it
-   					// or it''s matching for the wrong url
-   					// who fuckin knows
-   					
-   					setTimeout(function(test){500+200;}, 1000);
+                {code:
+                  // here is where we'd change the option to choose a different domain.
+                  "var domainSelectButton = document.querySelectorAll(\"[title = 'streamplay.to']\")[0]; \
+                  domainSelectButton.click();"},
 
-   					chrome.tabs.query({'url': ['*://*.swatchseries.to/freecale']}, function(newWatchTab) {
-   						var newWatchTabID = newWatchTab[0].id;
-   						chrome.tabs.executeScript(newWatchTabID, {code:
-   							"var clickToWatch = document.getElementsByClassName('push_button blue')[0]; \
-   							clickToWatch.click();"
-   						});
-   					});
-   				});
-   			});
-		};
+                // run this function after clicking the streamplay link
+                function(test){
+                  // so the reason this is here is because if I don't do it in a callback
+                  // it will just return a random ass tab
+                  // because javascript is asynchronous
+                  chrome.tabs.query({'url': '*://*.swatchseries.to/freecale*'}, function(newWatchTab) {
+                    // (Hopefully), gets the tab ID of the "click here to play" tab, and clicks here
+                    // to play.
+                    var newWatchTabID = newWatchTab[0].id;
+                    // gets last tab with a watchseries url, which should be the click to play page
+                    chrome.tabs.executeScript(newWatchTabID, 
+
+                      {code:
+                        "var clickToWatch = document.getElementsByClassName('push_button blue')[0]; \
+                        clickToWatch.click();"},
+
+                      // run this function after clicking the click here to play button.
+                      function(test1){
+                        11 + 5;
+                      });
+                  });
+                });
+              // here so that it only clicks the domain link once, rather than every time the tab is updated.
+              domainSelectTabUpdated = true;
+
+            };
+          });
+ 				});
+   		});
+    };
 	});
 };
 
